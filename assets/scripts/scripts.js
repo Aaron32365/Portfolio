8d166d86b0060942bec6146e92f644093483e937
Aaron32365 <56372224+Aaron32365@users.noreply.github.com>
2020-02-29 23:52:21 -0800

    revamp of entire portfolio, landing section created

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,16 +1,4 @@
 /*
-let i = 0;
-function typeWriter() {
-  var txt = "This is a test"
-  var speed = 150;
-  if (i < txt.length) {
-    document.getElementById("hello-world").innerHTML += txt.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed);
-  }
-}
-*/
-
 var $window = $(window);
 var $elem = $(".typeWrite")
 
@@ -27,8 +15,42 @@ var $elem = $(".typeWrite")
 $(document).on("scroll", function () {
     if (isScrolledIntoView($elem, $window)) { //if the element is in view, typewrite animation
       $elem.addClass("animateTypeWrite")
-      $elem.show()
     }
 })
+console.log($(".intro-sub"))
 
-console.log($(".typeWrite").text())
+if(isScrolledIntoView($(".intro-sub"), $window)){
+  setTimeout(function(){
+    $(".intro-sub").addClass("animateTypeWrite")
+  }, 4000)
+}
+
+
+*/
+
+console.log($(".intro-sub").text())
+
+
+///////// a new begenning ////////
+
+function printSentence(id, sentence, speed) {
+  var index = 0,
+      timer = setInterval(function() {
+    var char = sentence.charAt(index);
+    if(char === '<') {
+      index = sentence.indexOf('>' , index)
+    }
+
+    document.getElementById(id).innerHTML = sentence.substr(0, index);
+    if(++index === sentence.length){
+      clearInterval(timer);
+    }
+  }, speed);
+} //printSentence
+
+printSentence(
+  'intro',
+  $("#intro").html(),
+  40
+);
+console.log($("#intro").text())
