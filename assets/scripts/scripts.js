2fc256699b58c25bf6ef4e84ed6199094b4ad1e0
Aaron32365 <56372224+Aaron32365@users.noreply.github.com>
2020-03-11 21:18:14 -0700

    minor html clean up

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,33 +1,47 @@
+const $window = $(window);
+const nav = $(".nav-link")
+let scrolled = false
 
-var $window = $(window);
-
-    function isScrolledIntoView($elem, $window) { //checking if element is in view
-        var docViewTop = $window.scrollTop();
-        var docViewBottom = docViewTop + $window.height();
-        var elemTop = $elem.offset().top;
-        var elemBottom = elemTop + $elem.height();
-
-        return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
-    }
+printSentence(
+  'intro',
+  $("#intro").html(),
+  38
+);
 
 $(document).on("load", $("#intro-section"),(event) => {
   event.preventDefault()
   $("#intro").removeAttr("display: none")
 })
 
-let scrolled = false
 $(document).on("scroll", $("#marker1"), function(event){
   event.preventDefault()
   if(isScrolledIntoView($("#marker1"), $window) && scrolled === false){
-      printSentence('about', $("#about").html(), 38)
-    scrolled = true
+      printSentence('about-main', $("#about-main").html(), 38)
+      scrolled = true
   }
 })
 
-
-$(document).on("click",$("#learnMore"),() => {
-  $("#about-section").scrollIntoView()
+$(document).on("click", $("#learnMore"),function(event){
+  event.preventDefault()
+  let el = document.getElementById("about-section");
+  el.scrollIntoView()
 })
+
+
+$(".nav-link").on("click", function(event){
+  event.preventDefault()
+  let el = document.getElementById(`${$(this)[0].id}-section`)
+  console.log(el)
+  el.scrollIntoView()
+})
+
+function isScrolledIntoView($elem, $window) { //checking if an element is in view
+    var docViewTop = $window.scrollTop();
+    var docViewBottom = docViewTop + $window.height();
+    var elemTop = $elem.offset().top;
+    var elemBottom = elemTop + $elem.height();
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+}
 
 function printSentence(id, sentence, speed) {
   var index = 0,
@@ -36,22 +50,9 @@ function printSentence(id, sentence, speed) {
     if(char === '<') {
       index = sentence.indexOf('>' , index)
     }
-
     document.getElementById(id).innerHTML = sentence.substr(0, index);
     if(++index === sentence.length){
       clearInterval(timer);
     }
   }, speed);
-} //printSentence
-
-printSentence(
-  'intro',
-  $("#intro").html(),
-  38
-);
-console.log($("#intro").text())
-
-$(document).on('click', $("#more-about"), event =>{
-  event.preventDefault()
-  
-})
+} 
